f5426a8c9859e0b5650700d765c80a523b32a44e
VANDINHHOAI <vanhhh332@gmail.com>
2024-12-01 21:02:24 +0700

    add new API

--- src/modules/invoicePayments/invoicePayment.module.ts
+++ src/modules/invoicePayments/invoicePayment.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { InvoicePaymentController } from "./invoicePayment.controller";
 import { InvoicePaymentService } from "./invoicePayment.service";
 import { InvoicePaymentEntity } from "src/entities/invoicePayments.entity";
+import { InvoiceModule } from "../invoice/invoice.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([InvoicePaymentEntity])],
+    imports: [TypeOrmModule.forFeature([InvoicePaymentEntity]), InvoiceModule],
     controllers: [InvoicePaymentController],
     providers: [InvoicePaymentService],
 })
